fbdb7228a0d6e7fb58dc2d98f1b115d7d50d00cb
liudonghua123 <liudonghua123@gmail.com>
2018-11-26 09:34:04 +0800

    integrate with monaco-editor, referenced https://github.com/Microsoft/monaco-editor/issues/82

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MonacoEditor from 'react-monaco-editor';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '// type your code...',
+    }
+  }
+  editorDidMount = (editor, monaco) => {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+  onChange = (newValue, e) => {
+    console.log('onChange', newValue, e);
+  }
   render() {
+    const code = this.state.code;
+    const options = {
+      selectOnLineNumbers: true
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MonacoEditor
+        width="800"
+        height="600"
+        language="javascript"
+        theme="vs-dark"
+        value={code}
+        options={options}
+        onChange={this.onChange}
+        editorDidMount={this.editorDidMount}
+      />
     );
   }
 }
